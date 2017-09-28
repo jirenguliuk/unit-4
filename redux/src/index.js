1ee2cd52c8448da8eb823297c7e99de4bec8332e
jirenguliuk <707529659@qq.com>
2017-09-29 00:22:31 +0800

    first commit

---
+++ redux/src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import ControlPanel from './views/ControlPanel.js';
+import './index.css';
+
+ReactDOM.render(
+  <ControlPanel />,
+  document.getElementById('root')
+);
